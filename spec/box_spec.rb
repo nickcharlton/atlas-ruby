3d5ed0cb1d7a75501f71e30d0f3645f60a653411
Nick Charlton <nick@nickcharlton.net>
2015-05-13 15:35:43 +0100

    Adds a spec for box testing.

---
+++ spec/box_spec.rb
@@ -0,0 +1,34 @@
+require File.expand_path 'spec_helper.rb', __dir__
+
+describe Atlas::Box do
+  before do
+    VCR.insert_cassette name
+
+    Atlas.configure do |config|
+      config.access_token = 'test-token'
+    end
+  end
+
+  after do
+    VCR.eject_cassette
+  end
+
+  it 'can fetch a box' do
+    box = Atlas::Box.load('atlas-ruby', 'example')
+
+    box.wont_be_nil
+    box.name.must_equal 'example'
+  end
+
+  it 'can build a box from a json response' do
+    hash = { 'name' => 'example', 'tag' => 'atlas-ruby/example',
+             'short_description' => '', 'description_html' => '',
+             'description_markdown' => '', 'username' => 'atlas-ruby',
+             'private' => '', 'created_at' => '', 'updated_at' => '' }
+    box = Atlas::Box.from_json(hash)
+
+    box.wont_be_nil
+    box.name.must_equal 'example'
+    box.tag.must_equal 'atlas-ruby/example'
+  end
+end
