bc2c47f959a50e14f498dfb4394d3df6363a5227
Nick Charlton <nick@nickcharlton.net>
2015-05-13 12:47:37 +0100

    Adds and configured VCR for recorded requests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'minitest/autorun'
 require 'minitest/pride'
 
+# pull in the VCR setup
+require File.expand_path './support/vcr_setup.rb', __dir__
+
 # pull in the library
 require 'atlas'
