96e124cd2bc72f94c6651a9400a5b6a4e600a094
Nick Charlton <nick@nickcharlton.net>
2015-07-23 12:19:30 +0100

    Adds inline documentation to Box.

--- lib/atlas/box.rb
+++ lib/atlas/box.rb
@@ -1,9 +1,23 @@
 module Atlas
   # Representation and handling of Box objects.
+  #
+  # @attr_accessor [String] name The name of the box
+  # @attr_accessor [String] username The owner of the box
+  # @attr_accessor [String] short_description A short version of the
+  #   description.
+  # @attr_accessor [String] description Markdown text used to describe it.
+  # @attr_accessor [Boolean] is_private A boolean if the box is private.
+  # @attr_accessor [Version] current_version the current version of this box.
+  # @attr_accessor [Array] versions versions associated with this box.
   class Box < Resource
     attr_accessor :name, :username, :short_description, :description,
                   :is_private, :current_version, :versions
 
+    # Find a box by it's tag.
+    #
+    # @param [String] tag the tag of the box.
+    #
+    # @return [Box] a representation of the box.
     def self.find(tag)
       url_builder = UrlBuilder.new tag
       response = Atlas.client.get(url_builder.box_url)
@@ -31,6 +45,20 @@ module Atlas
       box
     end
 
+    # Initialize a box from a tag and object hash.
+    #
+    # @param [String] tag the tag which represents the origin of the box.
+    # @param [Hash] hash the attributes for the box
+    # @param hash [String] :name The name of the box, used to identify it.
+    # @option hash [String] :username The username to assign the box to.
+    # @option hash [String] :short_description The short description is used on
+    #   small box previews.
+    # @option hash [String] :description Markdown text used as a full-length and
+    #   in-depth description of the box.
+    # @option hash [Boolean] :is_private A boolean if the box should be private
+    #   or not.
+    #
+    # @return [Box] a new box object.
     def initialize(tag, hash = {})
       hash['is_private'] = hash['private']
       hash['description'] = hash['description_markdown']
@@ -38,6 +66,12 @@ module Atlas
       super(tag, hash)
     end
 
+    # Assign the current version.
+    #
+    # @note This is intended to be used to assign the response object, not
+    #   calling directly. Calling `release` will set the `current_version`.
+    #
+    # @param [Hash] attr a response hash of a `BoxVersion`.
     def current_version=(attr)
       if attr.is_a? Hash
         @current_version = BoxVersion.new("#{tag}/#{attr['version']}", attr)
@@ -46,12 +80,21 @@ module Atlas
       end
     end
 
+    # Assign the versions of this box.
+    #
+    # @note This is intended to be used to assign the response object, not
+    #   calling directly. You could do something unexpected by replacing this.
+    #
+    # @param [Array] attr an array of responses hashes.
     def versions=(attr)
       @versions = attr.collect do |v|
         BoxVersion.new("#{tag}/#{v['version']}", v)
       end
     end
 
+    # Save the box.
+    #
+    # @return [Hash] Atlas response object.
     def save
       body = { box: to_hash }
 
